392c80b9bfe9ab96afac001fc44a965b70eeae5e
Justin Crain <131824461+Jcrain5418@users.noreply.github.com>
2023-11-28 16:30:09 -0600

    buttons clickable with display divs and movie results

--- movie.js
+++ movie.js
@@ -1,13 +1,57 @@
-const helpBtn = document.querySelector('.helpBtn')
+const chooseBtn = document.querySelector('.decide_button')
+const helpBtn = document.querySelector('.help_button')
 const closeBtn = document.querySelector('.closeBtn')
-const dialog = document.querySelector('dialog')
+const formElm = document.querySelector('#form_div')
+const movieDis = document.querySelector('#movie_display')
+const movieText = document.querySelector('[movie-result-display]')
+const thanksBtn = document.querySelector('.thanks')
+const tryAgain = document.querySelector('.choose_again')
 
+const randArray = [
+    'The Thing', 'Prince Of Darkness', 'Into The Mouth Of Madness'
+]
+
+chooseBtn.addEventListener('click', ()=> {
+    console.log('clicked')
+    movieDis.classList.add('show')
+    //picks from array of stored movies and displays it
+    pickAtRandom()
+})
+
+tryAgain.addEventListener('click', () => {
+    console.log('clicked')
+    pickAgain()
+})
+
+function pickAtRandom() {
+    const randomIndex = Math.floor(Math.random() * randArray.length)
+    const answer = randArray[randomIndex]
+    console.log(answer)
+    movieText.innerText = answer
+}
+
+function pickAgain() {
+    movieText.innerText = ''
+    const randomIndex = Math.floor(Math.random() * randArray.length)
+    const answer = randArray[randomIndex]
+    console.log(answer)
+    movieText.innerText = answer
+}
+
+
+
+
+//open and close event listeners
+thanksBtn.addEventListener('click', () => {
+    console.log('clicked')
+    movieDis.classList.remove('show')
+})
 
 helpBtn.addEventListener('click', () => {
-    dialog.showModal()
-    //dialog.classList.add('.show')
+    formElm.classList.add('show')
+    
 })
 
 closeBtn.addEventListener('click', () =>{
-    dialog.close()
+    formElm.classList.remove('show')
 })
